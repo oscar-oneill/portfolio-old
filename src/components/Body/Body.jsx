fcd16ca92e1619d5c284e12cf3f40c6374d0918d
Oscar O'Neill <theoscaroneill@aol.com>
2020-12-19 01:40:59 -0500

    minor updates

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -11,12 +11,12 @@ const Body = () => {
             <span className="content__text">Yesterday</span>
             <p className="content__info">Music website</p>
             <div className="content__links">
-              <a href="https://yesterday.netlify.app/">
+              <a target="_blank" href="https://yesterday.netlify.app/" rel="noreferrer noopener">
                 <img src={internet} alt="internet logo" />
                 &nbsp; Website
               </a>
 
-              <a href="https://github.com/oscar-oneill/yesterday">
+              <a target="_blank" href="https://github.com/oscar-oneill/yesterday-redesign" rel="noreferrer noopener">
                 <img src={github} alt="github logo" />
                 &nbsp; Github
               </a>
@@ -27,12 +27,12 @@ const Body = () => {
             <span className="content__text">Telepath</span>
             <p className="content__info">Reddit video & gif viewer</p>
             <div className="content__links">
-              <a href="https://telepath.netlify.app/">
+              <a target="_blank" href="https://telepath.netlify.app/" rel="noreferrer noopener">
                 <img src={internet} alt="internet logo" />
                 &nbsp; Website
               </a>
 
-              <a href="https://github.com/oscar-oneill/telepath">
+              <a target="_blank" href="https://github.com/oscar-oneill/telepath" rel="noreferrer noopener">
                 <img src={github} alt="github logo" />
                 &nbsp; Github
               </a>
@@ -43,12 +43,12 @@ const Body = () => {
             <span className="content__text">Three Sports Men Podcast*</span>
             <p className="content__info">Podcast website</p>
             <div className="content__links">
-              <a href="https://threesportsmen.netlify.app/">
+              <a target="_blank" href="https://threesportsmen.netlify.app/" rel="noreferrer noopener">
                 <img src={internet} alt="internet logo" />
                 &nbsp; Website
               </a>
 
-              <a href="https://github.com/oscar-oneill/threesportsmen">
+              <a target="_blank" href="https://github.com/oscar-oneill/threesportsmen" rel="noreferrer noopener">
                 <img src={github} alt="github logo" />
                 &nbsp; Github
               </a>
@@ -59,12 +59,12 @@ const Body = () => {
             <span className="content__text">Splashgram</span>
             <p className="content__info">Instagram-styled Unsplash viewer</p>
             <div className="content__links">
-              <a href="https://splashgram.herokuapp.com/">
+              <a target="_blank" href="https://splashgram.herokuapp.com/" rel="noreferrer noopener">
                 <img src={internet} alt="internet logo" />
                 &nbsp; Website
               </a>
 
-              <a href="https://github.com/oscar-oneill/splashgram">
+              <a target="_blank" href="https://github.com/oscar-oneill/splashgram" rel="noreferrer noopener">
                 <img src={github} alt="github logo" />
                 &nbsp; Github
               </a>
@@ -75,12 +75,12 @@ const Body = () => {
             <span className="content__text">ExerTracker</span>
             <p className="content__info">Exercise tracker</p>
             <div className="content__links">
-              <a href="http://exercise-tracker-react.herokuapp.com/">
+              <a target="_blank" href="http://exercise-tracker-react.herokuapp.com/" rel="noreferrer noopener">
                 <img src={internet} alt="internet logo" />
                 &nbsp; Website
               </a>
 
-              <a href="https://github.com/oscar-oneill/exertracker">
+              <a target="_blank" href="https://github.com/oscar-oneill/exertracker" rel="noreferrer noopener">
                 <img src={github} alt="github logo" />
                 &nbsp; Github
               </a>
