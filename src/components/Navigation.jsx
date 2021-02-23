bb172410bde9570c2463a315c86bbda3c13214e5
Oscar O'Neill <theoscaroneill@aol.com>
2021-02-23 16:25:13 -0500

    Style Overhaul

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import '../styles/Navigation.css';
 import res from '../media/documents/oscaroneill_resume.pdf';
 
