8bfc6268ee1e623867271a1c8b5d0692fe282946
Oscar O'Neill <theoscaroneill@aol.com>
2021-12-12 15:21:10 -0500

    Minor Update

--- src/routes/Blogs.js
+++ src/routes/Blogs.js
@@ -1,14 +1,16 @@
 import React, {useState, useEffect, useContext} from 'react'
 import axios from 'axios'
 import Posts from '../Components/Posts'
-import BlogNavigation from '../Components/BlogNavigation';
+import BlogNavigation from '../Components/BlogNavigation'
 import { AuthContext } from '../Context/AuthContext'
 import BlogConnection from '../API/BlogConnection'
+import loading from '../media/images/loading.gif'
 
 const Blogs = () => {
     // eslint-disable-next-line
     const {isAuthenticated, setIsAuthenticated} = useContext(AuthContext)
-    const [blogs, setBlogs] = useState("");
+    const [blogs, setBlogs] = useState("")
+    const [loaded, setLoaded] = useState(false)
 
     document.title = "Oscar O'Neill | Blog";
 
@@ -21,30 +23,41 @@ const Blogs = () => {
             BlogConnection.get('/posts/all')
             .then(res => {
                 setBlogs(res.data)
+                setLoaded(true)
             })
         } catch (err) {
             console.error(err.message)
         }
     }
 
-    return (
-        <div>
-            <BlogNavigation/>
+    if (!loaded) {
+        return (
             <div className="main_container">
-                {blogs && blogs.map((blog) => (
-                    <Posts
-                        key={blog._id}
-                        id={blog._id}
-                        title={blog.post_title}
-                        blogger={blog.blogger}
-                        bgImage={blog.background_image}
-                        description={blog.description}
-                        created={blog.created}
-                    />
-                ))}
+                <div className="gif_container">
+                    <img className="loadingGIF" src={loading} alt="loading"/>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }   else {  
+            return (
+                <div>
+                    <BlogNavigation/>
+                    <div className="main_container">
+                        {blogs && blogs.map((blog) => (
+                            <Posts
+                                key={blog._id}
+                                id={blog._id}
+                                title={blog.post_title}
+                                blogger={blog.blogger}
+                                bgImage={blog.background_image}
+                                description={blog.description}
+                                created={blog.created}
+                            />
+                        ))}
+                    </div>
+                </div>
+            )
+        }
+    }
 
 export default Blogs
