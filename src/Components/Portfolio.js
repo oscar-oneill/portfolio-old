e0c3141242ec4ca96c8ce57c8bcc91c9bbf1e06c
Oscar O'Neill <theoscaroneill@aol.com>
2022-04-14 00:08:18 -0400

    updated portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useContext, useRef } from 'react'
 import '../styles/Portfolio.css'
 import Projects from './Projects'
-import { image } from '../Hooks/Images'
+import { projects } from '../utils/helpers'
 import { AppContext } from '../Context/AppContext'
 
 const Portfolio = () => {
@@ -18,73 +18,23 @@ const Portfolio = () => {
             offsetTop: offsetTop
         })
 
-    }, [portfolioInnerRef])
+    }, [portfolioInnerRef, setPortfolioData])
 
     return (
         <div className="portfolio__container" ref={portfolioInnerRef} id="portfolio">
-
-            <Projects
-                title='yesterday'
-                memo='A place to stream music from the album yesterday'
-                project={image.yesterday}
-                tech='React'
-                code='https://github.com/oscar-oneill/yesterday-redesign'
-                website='https://yesterday.netlify.app/'
-            />
-
-            <Projects
-                title='Splashgram'
-                memo='Splashgram is an Instagram styled Unsplash web application'
-                project={image.splashgram}
-                tech='React, Node, Express, Docker'
-                code='https://github.com/oscar-oneill/splashgram'
-                website='http://splashgram.xyz/'
-            />
-
-            <Projects
-                title='Switch'
-                memo='Get the best swap options when trading crypto'
-                project={image.switchswap}
-                tech='React, TypeScript, Web3.js'
-                code='https://github.com/oscar-oneill/switchswap'
-                website='https://switchswapcrypto.netlify.app/'
-            />
-
-            <Projects
-                title='Minimalist NFT'
-                memo='Mint an ultra minimal NFT on Ethereum*'
-                project={image.nft}
-                tech='React, Solidity, Ethers.js, Hardhat'
-                code='https://github.com/oscar-oneill/minimalist-nft'
-                website='https://minimalist-nft.vercel.app/'
-            />
-
-            <Projects
-                title="Oscar's Blog"
-                memo='My Web Development Blog'
-                project={image.blog}
-                tech='React, Node, Express, MongoDB'
-                code='https://github.com/oscar-oneill/portfolio'
-                website='https://oscaroneill.xyz/blog'
-            />
-
-            <Projects
-                title='Retro Tetris'
-                memo='Mintbean Hackathon Project for August 2021'
-                project={image.tetris}
-                tech='React, Styled Components'
-                code='https://github.com/oscar-oneill/tetris'
-                website='https://retrotetris.vercel.app'
-            />
-
-            <Projects
-                title='Blackjack'
-                memo='Mintbean Hackathon Project for July 2021'
-                project={image.blackjack}
-                tech='HTML, CSS, JavaScript'
-                code='https://github.com/oscar-oneill/blackjack'
-                website='https://blackjack21.vercel.app'
-            />
+            {projects && projects.map((project, i) => {
+                return (
+                    <Projects
+                        key={i}
+                        title={project.title}
+                        memo={project.memo}
+                        project={project.project}
+                        tech={project.tech}
+                        code={project.code}
+                        website={project.website}
+                    />
+                )
+            })}
         </div>
     )
 }
---
+++ src/utils/helpers.js
@@ -0,0 +1,61 @@
+import { image } from '../Hooks/Images';
+
+export const projects = [
+    {
+        "title": "yesterday",
+        "memo": "A place to stream music from the album yesterday",
+        "project": image.yesterday,
+        "tech": "React",
+        "code": "https://github.com/oscar-oneill/yesterday-redesign",
+        "website": "https://yesterday.netlify.app/"
+    },
+    {
+        "title": 'Splashgram',
+        "memo": 'Splashgram is an Instagram styled Unsplash web application',
+        "project": image.splashgram,
+        "tech": 'React, Node, Express, Docker',
+        "code": 'https://github.com/oscar-oneill/splashgram',
+        "website": 'http://splashgram.xyz/',
+    },
+    {
+        "title": 'Switch',
+        "memo": 'Get the best swap options when trading crypto',
+        "project": image.switchswap,
+        "tech": 'React, TypeScript, Web3.js',
+        "code": 'https://github.com/oscar-oneill/switchswap',
+        "website": 'https://switchswapcrypto.netlify.app/',
+    },
+    {
+        "title": 'Minimalist NFT',
+        "memo": 'Mint an ultra minimal NFT on Ethereum*',
+        "project": image.nft,
+        "tech": 'React, Solidity, Ethers.js, Hardhat',
+        "code": 'https://github.com/oscar-oneill/minimalist-nft',
+        "website": 'https://minimalist-nft.vercel.app/',
+    },
+    {
+        "title": "Oscar's Blog",
+        "memo": 'My Web Development Blog',
+        "project": image.blog,
+        "tech": 'React, Node, Express, MongoDB',
+        "code": 'https://github.com/oscar-oneill/portfolio',
+        "website": 'https://oscaroneill.xyz/blog',
+    },
+    {
+        "title": 'Retro Tetris',
+        "memo": 'Mintbean Hackathon Project for August 2021',
+        "project": image.tetris,
+        "tech": 'React, Styled Components',
+        "code": 'https://github.com/oscar-oneill/tetris',
+        "website": 'https://retrotetris.vercel.app',
+    },
+    {        
+        "title": 'Blackjack',
+        "memo": 'Mintbean Hackathon Project for July 2021',
+        "project": image.blackjack,
+        "tech": 'HTML, CSS, JavaScript',
+        "code": 'https://github.com/oscar-oneill/blackjack',
+        "website": 'https://blackjack21.vercel.app'
+    }
+]
+
